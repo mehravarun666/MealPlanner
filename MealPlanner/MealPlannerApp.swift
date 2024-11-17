71725bdc9adf23b930a4e5dd9d63f930e6e4422f
mehravarun666 <mehra.varun666@gmail.com>
2024-11-17 23:40:12 +0530

    Initial Commit

---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by Varun Mehra on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealPlannerTests/MealPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerTests.swift
+//  MealPlannerTests
+//
+//  Created by Varun Mehra on 17/11/24.
+//
+
+import Testing
+@testable import MealPlanner
+
+struct MealPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
